07e8f3da0f6ad93d7b177f552788c89306534a5e
GitDavid18 <david.schmitt@rwth-aachen.de>
2021-01-28 10:48:01 -0500

    C: Week 1 finished

--- Coding for Everyone C and C++/C for everybody 1/test.c
+++ Coding for Everyone C and C++/C for everybody 1/test.c
@@ -3,13 +3,14 @@
 #define PI 3.14159
 
 int main(){
-    printf("Hello\n");
+    printf("Hello\n
+    My name is David");
 
-    double area = 0.0, radius = 0.0;
-    printf("Enter radius: ");
-    scanf("%lf", &radius);
-    area = PI * radius * radius ;
-    printf("radius of %lf meters, area is %lf sq. meters\n", radius, area);
+    // double area = 0.0, radius = 0.0;
+    // printf("Enter radius: ");
+    // scanf("%lf", &radius);
+    // area = PI * radius * radius ;
+    // printf("radius of %lf meters, area is %lf sq. meters\n", radius, area);
 
 
     return 0;
