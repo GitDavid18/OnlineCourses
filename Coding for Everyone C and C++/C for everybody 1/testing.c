ec4b2c77ba46db8009b8d4df0b6cf47070d4d03a
David Schmitt <david.schmitt@rwth-aachen.de>
2021-02-02 16:46:36 -0500

    C1: Week 5 created

---
+++ Coding for Everyone C and C++/C for everybody 1/testing.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+//Fibonaci numbers
+// F(n) = F(n - 1) + F(n - 2)
+long rec_fibonaci(int n){
+    if(n <= 1){
+        return n;
+    }else{
+        //printf("%i\n", n);
+        long erg = rec_fibonaci(n - 1) + rec_fibonaci(n - 2);
+        // printf("erg = %ld\n", erg);
+        return (erg);
+    }
+
+}
+
+int main(void){
+    int n = 24;
+    for (int i = 1; i <= n; i++){
+        printf("%d -> %ld\n", i, rec_fibonaci(i));
+    }
+    
+
+    return 0;
+}
