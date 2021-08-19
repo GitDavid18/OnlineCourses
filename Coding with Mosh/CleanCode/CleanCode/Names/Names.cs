f09f9ad2ef4d169d3bc2f8be71d0455d50c7a049
David <dschmitt@angstromengineering.com>
2021-08-19 10:31:07 -0400

    CwM - Clean Code added
    Code by Mosh, creator of the course

---
+++ Coding with Mosh/CleanCode/CleanCode/Names/Names.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace CleanCode.Names
+{
+    public class Names
+    {
+        public Bitmap Method1(string n)
+        {
+            var b = new Bitmap(n);
+            var g = Graphics.FromImage(b);
+            g.DrawString("a", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 0));
+            g.DrawString("b", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 20));
+            g.DrawString("c", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 30));
+            return b;
+        }
+    }
+}
