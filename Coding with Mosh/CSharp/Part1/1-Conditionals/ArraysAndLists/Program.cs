f4c9e344e1c2c2ff23166ce8c41e326766d3b769
David Schmitt <david.schmitt@rwth-aachen.de>
2021-12-16 18:19:40 -0500

    CwM - CSharp 1: Excercise 3 3-5

--- Coding with Mosh/CSharp/Part1/1-Conditionals/ArraysAndLists/Program.cs
+++ Coding with Mosh/CSharp/Part1/1-Conditionals/ArraysAndLists/Program.cs
@@ -1,23 +1,22 @@
 ﻿//Note: For any of these exercises, ignore input validation unless otherwise directed. Assume the user enters values in the format that the program expects.
 
 
-//3- Write a program and ask the user to enter 5 numbers. If a number has been previously entered, display an error message and ask the user to re-try. Once the user successfully enters 5 unique numbers, sort them and display the result on the console.
 
-//4- Write a program and ask the user to continuously enter a number or type "Quit" to exit. The list of numbers may include duplicates. Display the unique numbers that the user has entered.
 
-//5- Write a program and ask the user to supply a list of comma separated numbers (e.g 5, 1, 9, 2, 10). If the list is empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try; otherwise, display the 3 smallest numbers in the list.
 public class ArraysAndLists
 {
     public static void Main(string[] args)
     {
         //Excercise 1
-        //FacebookSimulator();
+        FacebookSimulator();
         //Excercise 2
         ReverseName();
         //Excercise 3
+        FiveSortedNumbers();
         //Excercise 4
+        UniqueNumbers();
         //Excercise 5
-
+        ThreeSmallest();
     }
 
     #region Excercise1
@@ -87,8 +86,103 @@ public class ArraysAndLists
         }
 
         Console.WriteLine("Your name in reverse = " + reverse);
-    } 
+    }
 
 
     #endregion Excercise2
+
+    #region Excercise3
+    /// <summary>
+    ///3- Write a program and ask the user to enter 5 numbers. If a number has been previously entered, display an error message and ask the user to re-try. Once the user successfully enters 5 unique numbers, sort them and display the result on the console.
+    /// </summary>
+    public static void FiveSortedNumbers()
+    {
+        var numbers = new List<int>();
+        var maxNumbers = 5;
+        for (int i = 0; i < maxNumbers;)
+        {
+            Console.WriteLine("Please enter a number that has not been used yet");
+            var input = int.Parse(Console.ReadLine());
+            if (numbers.Contains(input))
+            {
+                Console.WriteLine("Number already in the list! Try again.");
+                continue;
+            }
+            else
+            {
+                numbers.Add(input);
+                i++;
+            }
+        }
+
+        numbers.Sort();
+
+        var output = "";
+        for (int i = 0;i < numbers.Count; i++)
+        {
+            output += numbers[i];
+            if (i != numbers.Count - 1)
+                output += " - ";
+        }
+        Console.WriteLine(output);
+    }
+    #endregion
+
+    #region Excercise4
+    /// <summary>
+    ///4- Write a program and ask the user to continuously enter a number or type "Quit" to exit. The list of numbers may include duplicates. Display the unique numbers that the user has entered.
+    /// </summary>
+    public static void UniqueNumbers()
+    {
+        var uniqueNumbers = new List<int>();
+        while (true)
+        {
+            Console.WriteLine("Please enter a number:");
+            var input = Console.ReadLine();
+            if (input == "Quit")
+                break;
+
+            var inputNumber = int.Parse(input);
+
+            if (!uniqueNumbers.Contains(inputNumber))
+                uniqueNumbers.Add(inputNumber);
+        }
+
+        Console.WriteLine("Unique Numbers entered: ");
+        foreach (var input in uniqueNumbers)
+            Console.WriteLine(input);
+    }
+    #endregion
+
+    #region Excercise5
+    /// <summary>
+    ///5- Write a program and ask the user to supply a list of comma separated numbers (e.g 5, 1, 9, 2, 10). If the list is empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try; otherwise, display the 3 smallest numbers in the list.
+    /// </summary>
+    public static void ThreeSmallest()
+    {
+        List<int> numbers = new List<int>();
+        while (true)
+        {
+            Console.WriteLine("Please enter a list of at least 5 numbers: ");
+            var input = Console.ReadLine();
+
+            var splitInput = input.Split(',');
+            if (splitInput.Length <= 4)
+                Console.WriteLine("Invalid amount of numbers. Please re-try.");
+            else
+            {
+                foreach (string x in splitInput)
+                    numbers.Add(int.Parse(x));
+                break;
+            }
+        }
+
+        Console.WriteLine("The 3 smallest numbers are: ");
+        for(int i = 0; i < 3; i++)
+        {
+            Console.WriteLine($"{numbers.Min()}");
+            numbers.Remove(numbers.Min());
+        }
+    }
+    #endregion
 }
