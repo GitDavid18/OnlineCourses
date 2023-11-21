03d2c604c8b86914c0a335e121f5762fa44ae527
David <david.schmitt@rwth-aachen.de>
2023-11-21 17:18:04 -0500

    CwM - Data Structures 1 - create a HashTable

---
+++ Coding with Mosh/DataStructures/src/HashTables/HashTable.java
@@ -0,0 +1,20 @@
+package HashTables;
+
+import java.util.LinkedList;
+
+public class HashTable {
+
+    // Collision Detection: Chaining
+    private class Entry{
+        public int key;
+        public String value;
+    }
+
+    private LinkedList<Entry>[] Entries;
+
+    public void put(int key, String value){}
+
+    public String get(int key){        return "";    }
+
+    public void remove(int key){}
+}
