0d8a206773169257f6586ecf8a6bc8181d63cc5a
AEDavid <dschmitt@angstromengineering.com>
2023-11-21 20:58:04 -0500

    HashTable implementation

--- Coding with Mosh/DataStructures/src/HashTables/HashTable.java
+++ Coding with Mosh/DataStructures/src/HashTables/HashTable.java
@@ -1,20 +1,74 @@
 package HashTables;
 
+import javax.swing.*;
 import java.util.LinkedList;
+import java.util.Map;
 
 public class HashTable {
+
+    private Integer length = 10;
 
     // Collision Detection: Chaining
     private class Entry{
         public int key;
         public String value;
+
+        public Entry(int key, String value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 
+    public HashTable(){
+        Entries = new LinkedList[length];
+    }
+
+    private Integer getHash(Integer key){
+        return key.hashCode() % length;
+    }
     private LinkedList<Entry>[] Entries;
 
-    public void put(int key, String value){}
+    public void put(int key, String value){
+        var index = getHash(key);
 
-    public String get(int key){        return "";    }
+        if(Entries[index] == null)
+            Entries[index] = new LinkedList<Entry>();
 
-    public void remove(int key){}
+        for (var entry : Entries[index]) {
+            if (entry.key == key) {
+                entry.value = value;
+                return;
+            }
+        }
+
+        Entries[index].addLast(new Entry(key, value));
+    }
+
+    public String get(int key){
+        var index = getHash(key);
+        if(Entries[index] == null)
+            return null;
+
+        for (var entry :Entries[index]){
+            if (entry.key == key)
+                return entry.value;
+        }
+
+        return null;
+    }
+
+    public void remove(int key){
+        var index = getHash(key);
+        if(Entries[index] == null)
+            throw new IllegalStateException();
+
+        for (var entry : Entries[index]){
+            if (entry.key == key) {
+                Entries[index].remove(entry);
+                return;
+            }
+        }
+
+        throw new IllegalStateException();
+    }
 }
