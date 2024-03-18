ce5c40b79e4cf925aee3bc97f8cb2cfd4df5c27a
AEDavid <dschmitt@angstromengineering.com>
2024-03-18 19:54:23 -0400

    CwM - Java Script
    - Course code

---
+++ Coding with Mosh/Java Script/index.js
@@ -0,0 +1,25 @@
+// First JS code
+console.log('Hello again');
+
+let name = "dAVID";
+let number = 0.3;
+const pi = 3.1415;
+
+console.log(name);
+console.log(number);
+console.log(pi);
+
+name = "name"
+console.log(typeof (name))
+console.log(name);
+
+name = 30
+console.log(typeof (name))
+console.log(name);
+
+
+let person = {
+    name: 'David',
+    age: 33
+}
+console.log(person);
---
+++ Coding with Mosh/Java Script/playground.js
@@ -0,0 +1,11 @@
+let test = {
+    prop1: 55.6,
+    prop2: 'other prop'
+}
+
+console.log(test);
+
+let colors = ['red', 'green'];
+colors[2] = 'blue';
+
+console.log(colors);
