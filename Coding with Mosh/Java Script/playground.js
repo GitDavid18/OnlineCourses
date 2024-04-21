2be8d47e46833a9e5e5a7a8eb4f7ef3c87f52ea3
David <david.schmitt@rwth-aachen.de>
2024-04-21 14:14:02 -0400

    some playing around

--- Coding with Mosh/Java Script/playground.js
+++ Coding with Mosh/Java Script/playground.js
@@ -9,3 +9,25 @@ let colors = ['red', 'green'];
 colors[2] = 'blue';
 
 console.log(colors);
+
+
+function greet(name){
+    console.log('Hello ' + name);    
+}
+
+greet('John');
+greet('Peter');
+
+function square(number){
+    return number * number;
+}
+
+console.log(square(5));
+console.log(square(7));
+
+
+let userColor = 'red';
+let defaultColor = 'blue';
+let currentColor = userColor || defaultColor;
+
+console.log(currentColor);
