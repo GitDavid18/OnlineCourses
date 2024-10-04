c73ed77f6f6b4554a83ac83473342baa8970ba45
AEDavid <dschmitt@angstromengineering.com>
2024-10-04 16:58:00 -0400

    merge cleanup

--- Coding with Mosh/Java Script/Excercises/Control Flow/IsLandScape.js
+++
@@ -1,5 +0,0 @@
-function isLandscape(height, width){
-    return width > height;
-}
-
-console.log(isLandscape(800, 1600))
--- Coding with Mosh/Java Script/Excercises/Control Flow/MaxOf2Numbers.js
+++
@@ -1,9 +0,0 @@
-function max(a,b) {
-    return a > b ? a : b;
-}
-
-let x = 24;
-let y = 19;
-
-let result = max(x, y);
-console.log(result);
